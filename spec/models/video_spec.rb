ef9c628403829e57d9a85c2421fcbb4b270892f8
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-01-19 20:34:43 -0500

    Add shoulda_makers to video and cateogry tests

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,15 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "The Godfather", description: "Mob Movie")
-    video.save
-    expect(Video.first).to eq(video)
-  end
-  
-  it "belongs to a category" do
-    drama = Category.create(name: "Drama")
-    monk = Video.create(title: "Monk", description: "A detective show!", category: drama)
-    expect(monk.category).to eq(drama)
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
