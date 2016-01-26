735772664b3c7b0afc7c1a50f751fcb69c2fc596
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-01-25 19:17:28 -0500

    Add user model and register page

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,15 @@ Myflix::Application.routes.draw do
   
   get '/home', to: 'videos#index'
   
+  get '/register', to: 'users#new'
+  
   resources :videos, only: :show do
     collection do
       get :search, to: 'videos#search'
     end
   end
+  
+  resources :users
   
   resources :categories, only: :show
 end
