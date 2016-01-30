d6d01b2f19b14af3f5c65383fefb824c8c771310
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-01-30 12:44:52 -0500

    Minor syntax changes to videos and reviews controller tests

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -9,21 +9,21 @@ describe VideosController do
       get :show, id: video.id
       expect(assigns(:video)).to eq(video)
     end
-  end
   
-  it "sets @reviews for authenticated users" do
-    session[:user_id] = Fabricate(:user).id
-    video = Fabricate(:video)
-    review1 = Fabricate(:review, video: video)
-    review2 = Fabricate(:review, video: video)
-    get :show, id: video.id
-    expect(assigns(:reviews)).to match_array([review1, review2])
-  end
-  
-  it "redirects user to the sign in page for unauthenticated user" do
-    video = Fabricate(:video)
-    get :show, id: video.id
-    expect(response).to redirect_to sign_in_path
+    it "sets @reviews for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      review1 = Fabricate(:review, video: video)
+      review2 = Fabricate(:review, video: video)
+      get :show, id: video.id
+      expect(assigns(:reviews)).to match_array([review1, review2])
+    end
+
+    it "redirects user to the sign in page for unauthenticated user" do
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(response).to redirect_to sign_in_path
+    end
   end
   
   describe "GET search" do
