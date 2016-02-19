8ff3368055d6d864f480f792570f7d28e87b0c80
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-02-19 18:19:56 -0500

    Add welcome email when a user signs up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
+      AppMailer.welcome_email(@user).deliver
       redirect_to sign_in_path
     else
       render 'new'
