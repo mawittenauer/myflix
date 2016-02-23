91b8703d49d76b328a22fc25b2c00ee8c8a6580a
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-02-22 19:57:03 -0500

    Refactor password reset and forgot password controller specs

--- spec/controllers/forgot_passwords_controller_spec.rb
+++ spec/controllers/forgot_passwords_controller_spec.rb
@@ -3,22 +3,24 @@ require 'spec_helper'
 describe ForgotPasswordsController do
   describe "POST create" do
     context "with blank input" do
+      
+      before { post :create, email: "" }
+      
       it "redirects to the forgot password page" do
-        post :create, email: ""
         expect(response).to redirect_to forgot_password_path
       end
       
       it "shows an error message" do
-        post :create, email: ""
         expect(flash[:danger]).to eq("Email cannot be blank.")
       end
     end
     
     context "with existing email" do
+            
       after { ActionMailer::Base.deliveries.clear }
       
       it "should redirect to the forgot password confirmation page" do
-        Fabricate(:user, email: "joe@example.com")
+        joe = Fabricate(:user, email: "joe@example.com")
         post :create, email: "joe@example.com"
         expect(response).to redirect_to forgot_password_confirmation_path
       end
@@ -30,20 +32,21 @@ describe ForgotPasswordsController do
       end
       
       it "sends out an email to the email address" do
-        Fabricate(:user, email: "joe@example.com")
+        joey = Fabricate(:user, email: "joe@example.com")
         post :create, email: "joe@example.com"
         expect(ActionMailer::Base.deliveries.last.to).to eq(["joe@example.com"])
       end
     end
     
     context "with non-existing email" do
+      
+      before { post :create, email: "foo@example.com" }
+      
       it "redirect to the forgot password page" do
-        post :create, email: "foo@example.com"
         expect(response).to redirect_to forgot_password_path
       end
       
       it "shows an error message" do
-        post :create, email: "foo@example.com"
         expect(flash[:danger]).to eq("There is no user with that email in the system.")
       end
     end
--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -21,37 +21,31 @@ describe PasswordResetsController do
   
   describe "POST create" do
     context "with valid token" do
-      it "redirects to the sign in page" do
-        alice = Fabricate(:user, password: 'old_password')
+      
+      let(:alice) { Fabricate(:user, password: 'old_password') }
+      
+      before do
         alice.update_column(:token, '12345')
         post :create, token: '12345', password: 'new_password'
+      end
+      
+      it "redirects to the sign in page" do
         expect(response).to redirect_to sign_in_path
       end
       it "updates the users password" do
-        alice = Fabricate(:user, password: 'old_password')
-        alice.update_column(:token, '12345')
-        post :create, token: '12345', password: 'new_password'
         expect(alice.reload.authenticate('new_password')).to be_truthy
       end
       it "sets the flash success message" do
-        alice = Fabricate(:user, password: 'old_password')
-        alice.update_column(:token, '12345')
-        post :create, token: '12345', password: 'new_password'
         expect(flash[:success]).to be_present
       end
       it "regenerates the users token" do
-        alice = Fabricate(:user, password: 'old_password')
-        alice.update_column(:token, '12345')
-        post :create, token: '12345', password: 'new_password'
         expect(alice.reload.token).not_to eq('12345')
       end
       it "deletes the users token" do
-        alice = Fabricate(:user, password: 'old_password')
-        alice.update_column(:token, '12345')
-        post :create, token: '12345', password: 'new_password'
         expect(alice.reload.token).to be_nil
       end
     end
+    
     context "with invalid token" do
       it "redirects to expired token path" do
         post :create, token: '12345', password: 'some_password'
