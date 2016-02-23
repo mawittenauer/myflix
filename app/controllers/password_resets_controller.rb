5ffdde8fc7ebc785eb5a5da48a73eba2563a24d6
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-02-23 09:36:05 -0500

    Add message for invalid password on password reset page

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -13,9 +13,15 @@ class PasswordResetsController < ApplicationController
     user = User.find_by(token: params[:token])
     if user
       user.password = params[:password]
-      user.delete_token
-      flash[:success] = "Your password has been changed. Please sign in."
-      redirect_to sign_in_path
+      @token = user.token
+      if user.save
+        user.delete_token
+        flash[:success] = "Your password has been changed. Please sign in."
+        redirect_to sign_in_path
+      else
+        flash.now[:danger] = "That is an invalid password, please make sure it is not blank and meets the password requirements."
+        render 'show'
+      end
     else
       redirect_to expired_token_path
     end
