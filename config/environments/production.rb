5b2c202a506c2f44789e57a668c067437b9bbc80
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-02-25 17:40:49 -0500

    Add mail gun to production environment for sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,13 +20,14 @@ Myflix::Application.configure do
   
   config.action_mailer.delivery_method = :smtp
   
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'vaci.lindsaar.net',
-    :user_name            => ENV['GMAIL_USERNAME'],
-    :password             => ENV['GMAIL_PASSWORD'],
-    :authentication       => 'plain',
-    :enable_starttls_auto => true
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'boiling-anchorage-51992.heroku.com',
+    :authentication => :plain,
   }
+  
+  ActionMailer::Base.delivery_method = :smtp
 end
