715872ec0c6142a634d7cbd733ee5fbf5991449c
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-03-04 17:10:44 -0500

    Add video url to videos and allow admin user to add it to a new video

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160304011047) do
+ActiveRecord::Schema.define(version: 20160304220329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20160304011047) do
     t.datetime "updated_at"
     t.string   "large_cover"
     t.string   "small_cover"
+    t.string   "video_url"
   end
 
 end
