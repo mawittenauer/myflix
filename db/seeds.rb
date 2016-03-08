89af0073b22195c4dfe9ce7dd9c49c09968309eb
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-03-08 10:55:24 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,18 @@
 comedies = Category.create(name: "Comedies")
 dramas = Category.create(name: "Dramas")
 
-Video.create(title: "Futurama", description: "space travel!", small_cover_url: "/tmp/futurama.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
-Video.create(title: "Monk", description: "Paranoid SF detective", small_cover_url: "/tmp/monk.jpg", category: dramas, large_cover_url: "/tmp/monk_large.jpg")
-Video.create(title: "Family Guy", description: "Peter Griffin and talking dog", small_cover_url: "/tmp/family_guy.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
-Video.create(title: "South Park", description: "Hippie kids", small_cover_url: "/tmp/south_park.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
-Video.create(title: "Futurama", description: "space travel!", small_cover_url: "/tmp/futurama.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
-monk = Video.create(title: "Monk", description: "Paranoid SF detective", small_cover_url: "/tmp/monk.jpg", category: dramas, large_cover_url: "/tmp/monk_large.jpg")
-Video.create(title: "Family Guy", description: "Peter Griffin and talking dog", small_cover_url: "/tmp/family_guy.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
-Video.create(title: "South Park", description: "Hippie kids", small_cover_url: "/tmp/south_park.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
-Video.create(title: "South Park", description: "Hippie kids", small_cover_url: "/tmp/south_park.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
+#Video.create(title: "Futurama", description: "space travel!", small_cover_url: "/tmp/futurama.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
+#Video.create(title: "Monk", description: "Paranoid SF detective", small_cover_url: "/tmp/monk.jpg", category: dramas, large_cover_url: "/tmp/monk_large.jpg")
+#Video.create(title: "Family Guy", description: "Peter Griffin and talking dog", small_cover_url: "/tmp/family_guy.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
+#Video.create(title: "South Park", description: "Hippie kids", small_cover_url: "/tmp/south_park.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
+#Video.create(title: "Futurama", description: "space travel!", small_cover_url: "/tmp/futurama.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
+#monk = Video.create(title: "Monk", description: "Paranoid SF detective", small_cover_url: "/tmp/monk.jpg", category: dramas, large_cover_url: "/tmp/monk_large.jpg")
+#Video.create(title: "Family Guy", description: "Peter Griffin and talking dog", small_cover_url: "/tmp/family_guy.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
+#Video.create(title: "South Park", description: "Hippie kids", small_cover_url: "/tmp/south_park.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
+#Video.create(title: "South Park", description: "Hippie kids", small_cover_url: "/tmp/south_park.jpg", category: comedies, large_cover_url: "/tmp/monk_large.jpg")
 
-kevin = User.create(full_name: "Kevin Wang", password: "password", email: "kevin@example.com")
+kevin = User.create(full_name: "Kevin Wang", password: "password", email: "kevin@example.com", admin: true)
 
-Review.create(user: kevin, video: monk, rating: 5, content: "This is a really good movie!")
-
-Review.create(user: kevin, video: monk, rating: 2, content: "This is a horrible movie.")
+#Review.create(user: kevin, video: monk, rating: 5, content: "This is a really good movie!")
+#
+#Review.create(user: kevin, video: monk, rating: 2, content: "This is a horrible movie.")
