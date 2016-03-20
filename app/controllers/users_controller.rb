735a466a0d3f96b14209d73062f1ecf9f6a8819f
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-03-20 18:00:00 -0400

    Add decorator for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :require_user, only: [:show]
   
   def show
-    @user = User.find(params[:id])
+    @user = UserDecorator.decorate(User.find(params[:id]))
   end
   
   def new
